82d1db0b816fab0bb8e77b0984b11e5c7834c489
Victor Abdukarimov <viredmi@gmail.com>
2023-01-25 14:34:30 +0300

    Optimize queries

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
 
-from foodcartapp.models import Product, Restaurant, Order
+from foodcartapp.models import Product, Restaurant, Cart 
 
 
 class Login(forms.Form):
@@ -93,5 +93,7 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     return render(request, template_name='order_items.html', context={
-        'orders': Order.objects.all(),
+
+        # TODO get order from cart
+        'order_items': Cart.objects.all().select_related('order'),
     })
