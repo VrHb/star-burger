9b2b8f8ea523b97c1905d6ed0eb097ebd05c51b9
Victor Abdukarimov <viredmi@gmail.com>
2023-02-01 18:44:59 +0300

    Added date information fields for order

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import F, Sum
 from django.core.validators import MinValueValidator
+from django.utils import timezone
 
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -161,6 +162,24 @@ class Order(models.Model):
         db_index=True
     )
     comment = models.TextField('Комментарий к заказу', blank=True)
+    registered_at = models.DateTimeField(
+        'Дата регистрации',
+        default=timezone.now,
+        blank=True,
+        db_index=True
+    )
+    called_at = models.DateTimeField(
+        'Дата звонка',
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    delivered_at = models.DateTimeField(
+        'Дата доставки',
+        null=True,
+        blank=True,
+        db_index=True
+    )
     objects = OrderQuerySet.as_manager()
 
     class Meta:
