815e1886148f97f9b7ee9198473788e8ade63b6c
Victor Abdukarimov <viredmi@gmail.com>
2023-02-14 17:30:37 +0300

    Rename query datetime field

--- locations/models.py
+++ locations/models.py
@@ -6,4 +6,4 @@ class Location(models.Model):
     address = models.CharField('Адрес места', max_length=200, unique=True)
     lat = models.DecimalField('Широта', max_digits=9, decimal_places=6)
     lon = models.DecimalField('Долгота', max_digits=9, decimal_places=6)
-    query_date = models.DateTimeField('Дата запроса')
+    query_at = models.DateTimeField('Дата запроса')
