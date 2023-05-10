ed9f76876e6e8457068551f97adce57cfb025eff
Victor Abdukarimov <viredmi@gmail.com>
2023-05-10 14:13:48 +0300

    Add code revision to rollbar message

--- deploy.sh
+++ deploy.sh
@@ -33,8 +33,9 @@ sudo systemctl restart star-burger.service
 
 
 echo "Telling rollbar about deploy ..."
+CODE_REVISION=git rev-parse --short HEAD
 source .env
-curl -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "'$ENVIRONMENT'", "revision": "1.0", "rollbar_name": "star-burger", "local_username": "'$USER'", "comment": "Deployed", "status": "succeeded"}'
+curl -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "'$ENVIRONMENT'", "revision": "'$CODE_REVISION'", "rollbar_name": "star-burger", "local_username": "'$USER'", "comment": "Deployed", "status": "succeeded"}'
 
 echo ""
 echo "All done!"
