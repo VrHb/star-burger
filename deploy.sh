2a7f92bc63ae1deffa805bcad0c43c428d885388
Victor Abdukarimov <viredmi@gmail.com>
2023-05-10 17:50:02 +0300

    Add fields witch get commit info for rollbar

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,7 @@ sudo systemctl restart star-burger.service
 echo "Telling rollbar about deploy ..."
 CODE_REVISION="$(git rev-parse --short HEAD)"
 source .env
-curl -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "'$ENVIRONMENT'", "revision": "'$CODE_REVISION'", "rollbar_name": "star-burger", "local_username": "'$USER'", "comment": "Deployed", "status": "succeeded"}'
+curl -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "'$ENVIRONMENT'", "payload": {"environment": "'$ENVIRONMENT'", "code_version": "'$(git rev-parse HEAD)'", "server": {"root": "file://users/VrHb/Documents/VrHb/star-burger/", "branch": "main"}}, "revision": "'$CODE_REVISION'", "rollbar_name": "star-burger", "local_username": "'$USER'", "comment": "Deployed", "status": "succeeded"}'
 
 echo ""
 echo "All done!"
