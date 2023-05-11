ea8be166e7be637914be6c74f2330c111fa0ccde
Victor Abdukarimov <viredmi@gmail.com>
2023-05-11 11:39:21 +0300

    Delete add host and creating python venv

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,7 @@ set -e
 echo "Pulling changes from repository ..."
 git pull origin main
 
-echo "Add host to ALLOWED_HOSTS env ..."
-echo "ALLOWED_HOSTS=$1" >> .env 
-
-echo "Creating virtual environment ..."
-python -m venv env
+echo "Activating virtual environment ..."
 source ./env/bin/activate
 
 echo "Instaling js packets ..."
@@ -30,7 +26,6 @@ python manage.py collectstatic --no-input
 
 echo "Restarting app unit ..."
 sudo systemctl restart star-burger.service
-
 
 echo "Telling rollbar about deploy ..."
 CODE_REVISION="$(git rev-parse --short HEAD)"
