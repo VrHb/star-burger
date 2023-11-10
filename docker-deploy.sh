ec614f88e4b2ae632579916c9c5fa2cf898ce976
Viktor Abdukarimov <viredmi@gmail.com>
2023-11-10 21:38:00 +0300

    Delete unused command and fix port

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -5,11 +5,8 @@ set -e
 echo "Pulling changes from repository ..."
 git pull origin main
 
-echo "Activating virtual environment ..."
-source ./env/bin/activate
-
 echo "Run docker containers ..."
-docker-compose buld && docker-compose up -d
+docker-compose build && docker-compose up -d
 
 echo "Copy static files from container ..."
 docker cp web-frontend:/usr/app/bundles staticfiles
