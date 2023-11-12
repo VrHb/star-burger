f5811a8e621b0d796eba409e350505bd044fb752
Viktor Abdukarimov <viredmi@gmail.com>
2023-11-13 00:16:03 +0300

    Add file docker-compose to compose command

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -9,7 +9,7 @@ echo "Run docker containers ..."
 docker-compose -f docker-compose.prod.yml up -d --build
 
 echo "Copy static files from container ..."
-docker-compose run back python manage.py collectstatic
+docker-compose -f docker-compose.prod.yml run back python manage.py collectstatic
 docker cp -a web-backend:/usr/src/app/staticfiles/. staticfiles
 docker cp -a web-frontend:/usr/src/app/bundles/. staticfiles
 cp -r assets/* staticfiles
