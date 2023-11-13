8d3986b3763f76362c21e99c33269fcf8b24c364
Viktor Abdukarimov <viredmi@gmail.com>
2023-11-13 20:33:26 +0300

    Fix parsel build and up containners

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -5,19 +5,12 @@ set -e
 echo "Pulling changes from repository ..."
 git pull origin main
 
-echo "Run docker images ..."
-docker-compose -f docker-compose.prod.yml build
-
-echo "Build frontend ..."
-docker run star-burger-front --name=web-frontend
-
-echo "Start backend container ..."
-docker-compose -f docker-compose.prod.yml up -d back
+echo "Run docker containers ..."
+docker-compose -f docker-compose.prod.yml --build up -d
 
 echo "Copy static files from container ..."
 docker exec -it web-backend python manage.py collectstatic
 docker cp -a web-backend:/usr/src/app/staticfiles/. staticfiles
 docker cp -a web-frontend:/usr/src/app/bundles/. staticfiles
-cp -r assets/* staticfiles
 
 echo "Done!"
