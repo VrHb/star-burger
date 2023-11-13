8116da417990766de0592eb2f30a46cad049bbac
Viktor Abdukarimov <viredmi@gmail.com>
2023-11-13 18:35:53 +0300

    Fix run containers

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -5,14 +5,19 @@ set -e
 echo "Pulling changes from repository ..."
 git pull origin main
 
-echo "Run docker containers ..."
-docker-compose -f docker-compose.prod.yml up -d --build
+echo "Run docker images ..."
+docker-compose -f docker-compose.prod.yml build
+
+echo "Build frontend ..."
+docker run star-burger-front --name=web-frontend
+
+echo "Start backend container ..."
+docker-compose -f docker-compose.prod.yml up -d back
 
 echo "Copy static files from container ..."
-docker-compose -f docker-compose.prod.yml run --rm back python manage.py collectstatic
+docker exec -it web-backend python manage.py collectstatic
 docker cp -a web-backend:/usr/src/app/staticfiles/. staticfiles
 docker cp -a web-frontend:/usr/src/app/bundles/. staticfiles
 cp -r assets/* staticfiles
-docker stop web-frontend
 
 echo "Done!"
